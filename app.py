fe3564fd1eca5ade1b429a7c4c0c612cb69243e7
Abhishek Kumar Singh <83462616+Abhishek5212@users.noreply.github.com>
2022-08-07 23:57:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,16 +57,16 @@ def review(text):
 
 def main():
    html_temp = """
-   <div class="" style="background-color:blue;" >
-   <div class="clearfix">           
-   <div class="col-md-12">
-   <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
-   <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center> 
-   <center><p style="font-size:25px;color:white;margin-top:10px;"Summer Internship 2022</p></center> 
-   </div>
-   </div>
-   </div>
-   """
+  <div class="" style="background-color:blue;" >
+  <div class="clearfix">           
+  <div class="col-md-12">
+  <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
+  <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center> 
+  <center><p style="font-size:25px;color:white;margin-top:10px;"Summer Internship 2022</p></center> 
+  </div>
+  </div>
+  </div>
+  """
 st.markdown(html_temp,unsafe_allow_html=True)
 st.header("Text review system")
   
@@ -91,16 +91,6 @@ if st.button("SVM"):
 if st.button("About"):
   st.subheader("Developed by Abhishek Kumar Singh")
   st.subheader("Student , Department of Computer Engineering")
-html_temp = """
-   <div class="" style="background-color:red;" >
-   <div class="clearfix">           
-   <div class="col-md-12">
-   <center><p style="font-size:20px;color:white;margin-top:10px;">Summer Internship 2022 Project-5 Deployment</p></center> 
-   </div>
-   </div>
-   </div>
-   """
-st.markdown(html_temp,unsafe_allow_html=True)
 
 if __name__=='__main__':
   main()
